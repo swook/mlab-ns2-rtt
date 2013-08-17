ce563bbdef4e32bd091377bc73800acc8ef94dca
Seon-Wook Park <seon.wook@swook.net>
2013-08-17 14:27:38 +0900

    Add memcaching of datastore queries

--- datastore.go
+++ datastore.go
@@ -19,19 +19,59 @@ package rtt
 import (
 	"appengine"
 	"appengine/datastore"
+	"appengine/memcache"
+	"fmt"
 	"net"
 )
 
 func DSGetClientGroup(c appengine.Context, ip net.IP) (*ClientGroup, error) {
-	cgIP := GetClientGroup(ip)
+	cgIP := GetClientGroup(ip).IP
 
-	rttKey := datastore.NewKey(c, "string", "rtt", 0, nil)
-	key := datastore.NewKey(c, "ClientGroup", cgIP.IP.String(), 0, rttKey)
+	var cg *ClientGroup
+	cg, err := mcGetClientGroup(c, cgIP)
+	switch err {
+	case memcache.ErrCacheMiss:
+		rttKey := datastore.NewKey(c, "string", "rtt", 0, nil)
+		key := datastore.NewKey(c, "ClientGroup", cgIP.String(), 0, rttKey)
 
-	var cg []ClientGroup
-	cg = make([]ClientGroup, 1)
-	if err := datastore.GetMulti(c, []*datastore.Key{key}, cg); err != nil {
+		var cgList []ClientGroup
+		cgList = make([]ClientGroup, 1)
+		if err := datastore.GetMulti(c, []*datastore.Key{key}, cgList); err != nil {
+			return nil, err
+		}
+		cg = &cgList[0]
+		mcSetClientGroup(c, cg)
+	case nil:
+		return cg, nil
+	default:
 		return nil, err
 	}
-	return &cg[0], nil
+	return cg, nil
+}
+
+func mcClientGroupKey(c appengine.Context, ip net.IP) string {
+	key := fmt.Sprintf("rtt:ClientGroup:%s", ip)
+	return key
+}
+
+func mcGetClientGroup(c appengine.Context, ip net.IP) (*ClientGroup, error) {
+	var cg ClientGroup
+	key := mcClientGroupKey(c, ip)
+	_, err := memcache.JSON.Get(c, key, &cg)
+	if err != nil {
+		return nil, err
+	}
+	return &cg, nil
+}
+
+func mcSetClientGroup(c appengine.Context, cg *ClientGroup) error {
+	item := &memcache.Item{
+		Key:    mcClientGroupKey(c, net.IP(cg.Prefix)),
+		Object: *cg,
+	}
+	err := memcache.JSON.Set(c, item)
+	if err != nil {
+		return err
+	}
+	return nil
 }
