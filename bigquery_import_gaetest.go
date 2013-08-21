7eff95221ad54abb3cc8d729417962a5739f2731
Seon-Wook Park <seon.wook@swook.net>
2013-08-21 16:15:16 +0900

    Remove temp. handler /rtt/build

--- bigquery_import_gaetest.go
+++ bigquery_import_gaetest.go
@@ -3,23 +3,15 @@
 package rtt
 
 import (
-	"fmt"
 	"net/http"
 )
 
 const (
-	URLBQTestBuild = "/rtt/build"
-	URLBQInit      = "/rtt/init"
+	URLBQInit = "/rtt/init"
 )
 
 func init() {
-	http.HandleFunc(URLBQTestBuild, bqbuild)
 	http.HandleFunc(URLBQInit, bqinit)
-}
-
-func bqbuild(w http.ResponseWriter, r *http.Request) {
-	bqImportDaily(w, r)
-	fmt.Fprintln(w, "Done")
 }
 
 func bqinit(w http.ResponseWriter, r *http.Request) {
