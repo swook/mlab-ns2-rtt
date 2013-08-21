ab8e58a395b1770c61fe0f1655adc2a17369ba30
Seon-Wook Park <seon.wook@swook.net>
2013-08-21 21:17:29 +0900

    Some necessary getter functions for rtt

--- ns/data/getters.go
+++ ns/data/getters.go
@@ -16,4 +16,54 @@
 
 package data
 
-import ()
+import (
+	"appengine"
+	"appengine/datastore"
+	"errors"
+	"math/rand"
+	"net"
+)
+
+var (
+	ErrNoMatchingSliverTool = errors.New("")
+)
+
+func GetSliverToolsWithToolID(c appengine.Context, toolID string) ([]*SliverTool, error) {
+	q := datastore.NewQuery("SliverTool").Filter("tool_id =", toolID)
+	var slivers []*SliverTool
+	if err := QueryData(c, toolID, q, slivers); err != nil {
+		return nil, err
+	}
+	return slivers, nil
+}
+
+func GetRandomSliverToolWithToolID(c appengine.Context, toolID string) (*SliverTool, error) {
+	slivers, err := GetSliverToolsWithToolID(c, toolID)
+	if err != nil {
+		return nil, err
+	}
+	idx := rand.Int() % len(slivers)
+	return slivers[idx], nil
+}
+
+func GetSiteWithSiteID(c appengine.Context, siteID string) (*Site, error) {
+	q := datastore.NewQuery("Site").Filter("site_id =", siteID)
+	var site *Site
+	if err := QueryData(c, siteID, q, site); err != nil {
+		return nil, err
+	}
+	return site, nil
+}
+
+func GetSliverToolWithIP(c appengine.Context, toolID string, ip net.IP) (*SliverTool, error) {
+	slivers, err := GetSliverToolsWithToolID(c, toolID)
+	if err != nil {
+		return nil, err
+	}
+	for _, s := range slivers {
+		if net.IP(s.SliverIPv4).Equal(ip) || net.IP(s.SliverIPv6).Equal(ip) {
+			return s, nil
+		}
+	}
+	return nil, ErrNoMatchingSliverTool
+}
